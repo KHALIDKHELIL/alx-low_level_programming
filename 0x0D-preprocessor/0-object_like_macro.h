c7da131cbc045160f4d402de1c89439d48b8a7a9
khalidkhelil <khalidkhelil19@gmail.com>
2022-04-12 07:22:42 +0300

    few

---
+++ 0x0D-preprocessor/0-object_like_macro.h
@@ -0,0 +1,24 @@
+#include "0-object_like_macro.h"
+#include "0-object_like_macro.h"
+#include <stdio.h>
+
+/**
+ *  * main - check the code
+ *   *
+ *    * Return: Always 0.
+ *     **/
+
+#define SIZE
+#define SIZE 1024
+
+
+
+
+int main(void)
+{
+	    int s;
+
+	        s = 98 + SIZE;
+		    printf("%d\n", s);
+		        return (0);
+}
---
+++ 0x0D-preprocessor/README.md
@@ -0,0 +1,1 @@
+preprocessor of c / c-p-p
