e392ab476c174df32183864ff9cb8355a4cef654
khalidkhelil <khalidkhelil19@gmail.com>
2022-04-12 08:07:07 +0300

    f

--- 0x0D-preprocessor/0-object_like_macro.h
+++ 0x0D-preprocessor/0-object_like_macro.h
@@ -1,26 +1,5 @@
-#include "0-object_like_macro.h"
-#include "0-object_like_macro.h"
-#include <stdio.h>
-
-/**
- *  * main - check the code
- *   *
- *    * Return: Always 0.
- *     **/
-
-#ifndef SIZE
-#define SIZE
+#ifndef MACRO_H
+#define MACRO_H SIZE
 #define SIZE 1024
 
 #endif
-
-
-
-int main(void)
-{
-	    int s;
-
-	        s = 98 + SIZE;
-		    printf("%d\n", s);
-		        return (0);
-}
---
+++ 0x0D-preprocessor/4-sum.h
@@ -0,0 +1,6 @@
+#ifndef LIKEMACRO_H 
+#define LIKEMACRO_H
+
+#define SUM(X,Y) (X + Y)
+
+#endif
